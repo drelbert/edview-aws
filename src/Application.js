afa0384f0c7d6366889274be5cca2bced0092c99
Dan Elbert <danrelbert@gmail.com>
2022-06-14 17:09:01 -0400

    update yellow to slate

--- src/Application.js
+++ src/Application.js
@@ -6,8 +6,8 @@ import { Page } from './Page';
 function Application() {
   return (
     <main className="h-screen">
-      <h1 className="text-3xl font-bold bg-yellow-300 p-4 border-b-2 border-yellow-400">
-        Incredibly Important Thoughts
+      <h1 className="text-3xl font-bold bg-slate-300 p-4 border-b-2 border-slate-400">
+        Incredibly Important 
       </h1>
       <div className="grid grid-cols-12 grid-rows-1 gap-4 h-full">
         <Navigation>
