0de75505ab3daabe7569c9662aa9ccd708473fb1
delbert <dev@mb-air.home>
2022-07-26 15:45:29 -0400

    update search with useEffect

--- src/SearchTalent.js
+++ src/SearchTalent.js
@@ -1,16 +1,36 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Skill from "./Skill";
 
-const SKILLS = ["engineering", "contracts", "innovation", "finance"];
+
+const ANIMALS = ["engineering", "contracts", "innovation", "finance"];
 
 const SearchTalent = () => {
   // original const, not mutable
-  // const skill = "Developer";
   // adding mutation with state via uesState("default value/state")
-  const [department, setDepartment] = useState("");
-  const [skill, setSkill] = useState("");
-  const [specialization, setSpecialization] = useState("");
-  const specializations = [];
-  // const [talent, setTalent] = useState([]);
+
+  const [ location, updateLocation] = useState("");
+  const [ animal, updateAnimal] = useState("");
+  const [breed, updateBreed] = useState("");
+  const [pets, setPets] = useState([]);
+  const breeds =[]
+
+
+
+  useEffect(() => {
+    requestSkills()
+  }, []); 
+
+  
+  async function requestSkills() {
+    const res = await fetch (
+      `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed${breed}`
+    )
+    const json = await res.json()
+
+    setPets(json.pets)
+
+  }
+
 
   return (
     <div>
@@ -23,35 +43,35 @@ const SearchTalent = () => {
       <div className="flex flex-row">
         <div className="basis-1/2 mt-2 mx-6 py-2 px-2  relative rounded-md shadow-lg border-2  text-xl text-gray-500">
           <form>
-            <label htmlFor="department">
-              Enter a Department
+            <label htmlFor="location">
+              Enter a Location
               <input
-                id="department"
-                value={department}
-                placeholder="department"
+                id="location"
+                value={location}
+                placeholder="location"
                 className="border-2  focus:ring-indigo-500 focus:border-indigo-500  my-6 block pl-2 pr-24 sm:text-sm border-gray-300 rounded-md"
-                onChange={(e) => setDepartment(e.target.value)}
+                onChange={(e) => updateLocation(e.target.value)}
               />
             </label>
             <div className="w-100">
-              <label htmlFor="skill">
-                Choose Skill
+              <label htmlFor="animal">
+                Choose Animal
                 <select
-                  id="skill"
-                  value={skill}
+                  id="animal"
+                  value={animal}
                   onChange={(e) => {
-                    setSkill(e.target.value);
-                    setSpecialization("");
+                    updateAnimal(e.target.value);
+                    updateBreed("");
                   }}
                   onBlur={(e) => {
-                    setSkill(e.target.value);
-                    setSpecialization("");
+                    updateAnimal(e.target.value);
+                    updateBreed("");
                   }}
                 >
                   <option />
-                  {SKILLS.map((skill) => (
-                    <option key={skill} value={skill}>
-                      {skill}
+                  {ANIMALS.map((animal) => (
+                    <option key={animal} value={animal}>
+                      {animal}
                     </option>
                   ))}
                 </select>
@@ -59,22 +79,23 @@ const SearchTalent = () => {
             </div>
 
             <div className="w-100">
-              <label htmlFor="specialization">
-                Choose Specialization
+              <label htmlFor="breed">
+                Choose Breed
                 <select
-                  id="specialization"
-                  value={specialization}
+                disabled={!breeds.length}
+                  id="breed"
+                  value={breed}
                   onChange={(e) => {
-                    setSpecialization(e.target.value);
+                    updateBreed(e.target.value);
                   }}
                   onBlur={(e) => {
-                    setSpecialization(e.target.value);
+                    updateBreed(e.target.value);
                   }}
                 >
                   <option />
-                  {specializations.map((allSpecializtion) => (
-                    <option key={allSpecializtion} value={allSpecializtion}>
-                      {allSpecializtion}
+                  {breeds.map((breed) => (
+                    <option key={breed} value={breed}>
+                      {breed}
                     </option>
                   ))}
                 </select>
@@ -90,7 +111,11 @@ const SearchTalent = () => {
           </form>
         </div>
         <div className="basis-1/2 mt-2 mx-6 py-2 px-2  relative rounded-md shadow-lg border-2  text-xl text-gray-500">
-          ToDo
+          Results: 
+          { pets.map(pet => (
+            <Skill name={pet.name} animal={pet.animal} breed={pet.breed} key={pet.id} />
+          ))}
+        
         </div>
       </div>
     </div>
---
+++ src/Skill.js
@@ -0,0 +1,11 @@
+const Skill = (props) => {
+    return (
+        <div>
+            <h1>{props.name}</h1>
+            <h2>{props.animal}</h2>
+            <h3>{props.breed}</h3>
+        </div>
+    )
+}
+
+export default Skill;
